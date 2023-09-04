5cfb6da90c81eeaee5b3672e4ffba0aba1a03c60
tantanquiaeo <98859541+tantanquiaeo@users.noreply.github.com>
2023-09-04 14:39:08 +0800

    color picker

--- index.js
+++ index.js
@@ -40,25 +40,26 @@ document
     );
   });
 
-  $(document).ready(function () {
-    // typing animation
-    (function ($) {
-      $.fn.writeText = function (content) {
-        var contentArray = content.split(""),
-          current = 0,
-          elem = this;
-        setInterval(function () {
-          if (current < contentArray.length) {
-            elem.text(elem.text() + contentArray[current++]);
-          }
-        }, 80);
-      };
-    })(jQuery);
-  
-    // input text for typing animation
-    $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
-  
-    // initialize wow.js
-    new WOW().init();
-  
+
+
+  $(function() {
+    //bg color selector
+    $(".color").click(function(){
+      var color = $(this).attr("data-value");
+      $("body").css("background-color", color);
+    });
+    
+    //add color picker if supported
+    if (Modernizr.inputtypes.color) {
+      $(".picker").css("display", 'inline-block');
+      var c = document.getElementById('colorpicker');
+      c.addEventListener('change', function(e) {
+        //d.innerHTML = c.value;
+        var color = c.value;
+        $("body").css("background-color", color);
+      }, false);
+    }
   });
+  function pickColor() {
+    $("#colorpicker").click();
+  }
