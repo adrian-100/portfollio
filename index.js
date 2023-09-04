64196d45f91f5ef40d97ae600247fb0562df7acd
tantanquiaeo <98859541+tantanquiaeo@users.noreply.github.com>
2023-09-04 14:02:36 +0800

    a

--- index.js
+++ index.js
@@ -39,3 +39,26 @@ document
       "_blank"
     );
   });
+
+  $(document).ready(function () {
+    // typing animation
+    (function ($) {
+      $.fn.writeText = function (content) {
+        var contentArray = content.split(""),
+          current = 0,
+          elem = this;
+        setInterval(function () {
+          if (current < contentArray.length) {
+            elem.text(elem.text() + contentArray[current++]);
+          }
+        }, 80);
+      };
+    })(jQuery);
+  
+    // input text for typing animation
+    $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
+  
+    // initialize wow.js
+    new WOW().init();
+  
+  });
